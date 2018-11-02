5377245dcd57a96376b1ef2d08df6faf4aa87bb2
olga.k <olga.koltsova@yahoo.com>
2018-11-02 23:04:36 +0300

    CheckResult Method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace hangman
 {
     class Program
     {
@@ -32,34 +32,43 @@ namespace ConsoleApp1
                 Console.Write("Word: {0} >>", hiddenWord);
                 string userGuess = Console.ReadLine();
 
-                if (selectedWord.Contains(userGuess))
-                {
-                    // at this point using selectedWord and hiddenWord strings like parallel arrays
-                    for(int j = 0; j < selectedWord.Length; j++)
-                    {
-                        if(selectedWord[j] == char.Parse(userGuess))
-                        {
-                            // replacing stars with letters
-                            hiddenWord = hiddenWord.Remove(j, 1);
-                            hiddenWord = hiddenWord.Insert(j, userGuess);
-                        }
-                    }
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Correct! Letter {0} exists in this word!", userGuess);
-                    Console.ResetColor();
-                }
-                else
+                if(hiddenWord == CheckResult(selectedWord, userGuess, hiddenWord))
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Oops! Letter {0} doesn't exists in this word!", userGuess);
                     Console.ResetColor();
                 }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct! Letter {0} exists in this word!", userGuess);
+                    Console.ResetColor();
+                }
+                hiddenWord = CheckResult(selectedWord, userGuess, hiddenWord);
+
             } while (hiddenWord != selectedWord);
 
             Console.WriteLine("Congratulations! You win! The word is {0}.", selectedWord);
+        }
 
-
-
+        private static string CheckResult(string selectedWord, string userGuess, string hiddenWord)
+        {
+            string result = hiddenWord;
+            if (selectedWord.Contains(userGuess))
+            {
+                // at this point using selectedWord and hiddenWord strings like parallel arrays
+                for (int j = 0; j < selectedWord.Length; j++)
+                {
+                    if (selectedWord[j] == char.Parse(userGuess))
+                    {
+                        // replacing stars with letters
+                        result = result.Remove(j, 1);
+                        result = result.Insert(j, userGuess);
+                    }
+                }
+            }
+            return result;
+            
         }
        
     }
